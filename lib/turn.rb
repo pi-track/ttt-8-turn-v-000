4701b62895ce312565303638223da1e4f4dbcd74
Patrick Eells <patrick.eells@gmail.com>
2018-01-04 15:54:27 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,3 +4,23 @@ def display_board(board)
   puts " ${board[3]} | ${board[4]} | ${board[5]} "
   puts "-----------"
   puts " ${board[6]} | ${board[7]} | ${board[8]} "
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == true
+    false
+  elsif index > board.size-1
+    false
+  else
+    true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
+end
+  
